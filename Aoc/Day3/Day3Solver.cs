c0937a9830f39615e1352dcde2ced8f632657b63
Hcagny <Hcagny@PCD5268>
2021-12-03 11:00:16 +0100

    Day3

---
+++ Aoc/Day3/Day3Solver.cs
@@ -0,0 +1,61 @@
+﻿using AdventOfCode.Data;
+
+namespace AdventOfCode.Day2;
+
+public static class Day3Solver
+{
+    public static long SolvePuzzle1()
+    {
+        var data = DataLoader.LoadDataPerLineFromDay(3)
+            .ToList();
+
+        var numberOfLines = data.Count;
+        var length = data[0].Length;
+
+        var gammabit = "";
+        var epsilonbit = "";
+
+        for (int i = 0; i < data[0].Length; i++)
+        {
+            var sum = data.Sum(s => int.Parse(s[i].ToString()));
+            gammabit += sum > numberOfLines / 2 ? 1 : 0;
+            epsilonbit += sum > numberOfLines / 2 ? 0 : 1;
+        }
+
+        return Convert.ToInt32(gammabit, 2) * Convert.ToInt32(epsilonbit, 2);
+    }
+
+    public static long SolvePuzzle2()
+    {
+        var data = DataLoader.LoadDataPerLineFromDay(3)
+            .ToList();
+
+
+        var oxygenRateList = DataLoader.LoadDataPerLineFromDay(3).ToList();
+        var Co2RateList = DataLoader.LoadDataPerLineFromDay(3).ToList();
+
+        for (int i = 0; i < data[0].Length; i++)
+        {
+            var oxygenRateListBit = oxygenRateList.Sum(s => int.Parse(s[i].ToString()));
+            var mostCommonBit = oxygenRateListBit * 2 >= oxygenRateList.Count ? 1 : 0;
+            oxygenRateList = oxygenRateList.Where(s => s[i].ToString() == mostCommonBit.ToString()).ToList();
+            if (oxygenRateList.Count == 1)
+            {
+                break;
+            }
+        }
+
+        for (int i = 0; i < data[0].Length; i++)
+        {
+            var Co2RateListBit = Co2RateList.Sum(s => int.Parse(s[i].ToString()));
+            var leastCommonBit = Co2RateListBit * 2 >= Co2RateList.Count ? 0 : 1;
+            Co2RateList = Co2RateList.Where(s => s[i].ToString() == leastCommonBit.ToString()).ToList();
+            if (Co2RateList.Count == 1)
+            {
+                break;
+            }
+        }
+
+        return Convert.ToInt32(oxygenRateList.Single(), 2) * Convert.ToInt32(Co2RateList.Single(), 2);
+    }
+}
--- Aoc/Program.cs
+++ Aoc/Program.cs
@@ -2,6 +2,6 @@
 using AdventOfCode.Day1;
 using AdventOfCode.Day2;
 
-Console.WriteLine($"{Day2Solver.SolvePuzzle2()}");
+Console.WriteLine($"{Day3Solver.SolvePuzzle2()}");
 
 
