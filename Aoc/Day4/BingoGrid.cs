5c1abe5db3da529278f2cbfa0ec665f2d6c5c82a
henri <henri@HENRI-PC>
2021-12-04 12:02:37 +0100

    begin implementation

---
+++ Aoc/Day4/BingoGrid.cs
@@ -0,0 +1,54 @@
+﻿namespace AdventOfCode.Day4
+{
+    public class BingoGrid
+    {
+        public int[] Grid { get; set; }
+
+        private readonly int _gridLength = 5;
+
+        public void CheckNumber(int bingoNumber)
+        {
+            foreach(var i in Grid)
+            {
+                if (i == bingoNumber)
+                {
+                    i == 0;
+                }
+            }
+        }
+
+        public bool IsGridSolved()
+        {
+            SolveDiagonal();
+            SolveHorizontalLines();
+            SolveVerticalLines();
+        }
+
+        private bool SolveDiagonal()
+        {
+
+        }
+
+        private bool SolveHorizontalLines()
+        {
+            for(var i=0;i< _gridLength; i++)
+            {
+                var sum = Grid.Where((x, i) => i % _gridLength == i);
+                if (sum.Sum() == 0) { return true; }
+            }
+
+            return false;
+        }
+
+        private bool SolveVerticalLines()
+        {
+            for (var i = 0; i < _gridLength; i++)
+            {
+                var sum = Grid.Skip().Take(5);
+                if (sum.Sum() == 0) { return true; }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Aoc/Day4/Day4Solver.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode.Data;
+
+namespace AdventOfCode.Day2;
+
+public static class Day4Solver
+{
+    public static long SolvePuzzle1()
+    {
+        var data = DataLoader.LoadDataPerLineFromDay(3)
+            .ToList();
+
+        return 0;
+    }
+
+    public static long SolvePuzzle2()
+    {
+        var data = DataLoader.LoadDataPerLineFromDay(3)
+            .ToList();
+
+        return 0;
+    }
+}
