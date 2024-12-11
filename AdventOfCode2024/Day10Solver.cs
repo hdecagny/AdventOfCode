16feff5a4ed261652bed3760f636e8ee9c9f7037
Hcagny <Hcagny@PCD5268>
2024-12-11 11:08:44 +0100

    Day 9-10

---
+++ AdventOfCode2024/Day10Solver.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode2024;
+
+using System.Numerics;
+
+public class Day10Solver : SolverBase2024
+{
+    private Dictionary<Complex, int> _map;
+
+    public override double SolvePuzzle1()
+    {
+        _map = LoadDataAsMapFromDay(10).ToDictionary(kvp => kvp.Key, kvp => int.Parse(kvp.Value.ToString()));
+
+        var trailheads = _map.Where(kvp => kvp.Value == 0).ToList();
+
+        return trailheads.Sum(CalculateTrailheadScore);
+    }
+    
+    public override double SolvePuzzle2()
+    {
+        _map = LoadDataAsMapFromDay(10).ToDictionary(kvp => kvp.Key, kvp => int.Parse(kvp.Value.ToString()));
+
+        var trailheads = _map.Where(kvp => kvp.Value == 0).ToList();
+
+        return trailheads.Sum(CalculateTrailheadRatinScore);
+    }
+
+    private int CalculateTrailheadScore(KeyValuePair<Complex, int> currentPosition)
+    {
+        var currentheight = 0;
+        var possiblePaths = new[] { currentPosition }.ToList();
+
+        while (currentheight < 9)
+        {
+            possiblePaths = possiblePaths.SelectMany(GetPossibleNext).ToList();
+            currentheight += 1;
+        }
+
+        return possiblePaths.Select(kvp=>kvp.Key).Distinct().Count();
+    }
+    
+    private int CalculateTrailheadRatinScore(KeyValuePair<Complex, int> currentPosition)
+    {
+        var currentheight = 0;
+        var possiblePaths = new[] { currentPosition }.ToList();
+
+        while (currentheight < 9)
+        {
+            possiblePaths = possiblePaths.SelectMany(GetPossibleNext).ToList();
+            currentheight += 1;
+        }
+
+        return possiblePaths.Count();
+    }
+
+    private IReadOnlyList<KeyValuePair<Complex, int>> GetPossibleNext(KeyValuePair<Complex, int> currentPosition)
+    {
+        return GetNeighbours(currentPosition.Key).Where(kvp => int.Parse(kvp.Value.ToString()) == currentPosition.Value + 1).ToList();
+    }
+
+    private IReadOnlyList<KeyValuePair<Complex, int>> GetNeighbours(Complex coordinate)
+    {
+        return _map.Where(k => new[]
+            {
+                coordinate + 1,
+                coordinate - 1,
+                coordinate + Complex.ImaginaryOne,
+                coordinate - Complex.ImaginaryOne
+            }.Contains(k.Key))
+            .ToList();
+    }
+}
---
+++ AdventOfCode2024/Day9Solver.cs
@@ -0,0 +1,128 @@
+﻿namespace AdventOfCode2024;
+
+public class Day9Solver : SolverBase2024
+{
+    public override double SolvePuzzle1()
+    {
+        var data = LoadDataFromDay(9).Select(c => int.Parse(c.ToString())).ToList();
+        var length = data.Count;
+
+        var filledGroups = Enumerable.Range(0, length)
+            .Where(i => i % 2 == 0)
+            .Select(i => data[i])
+            .ToList();
+        
+        var idOrdered = GetIdNumberOrdered(filledGroups);
+        var refragementedfile = GetRefragmentedFile(idOrdered, data);
+
+        return refragementedfile.Sum(kvp=> kvp.Key * kvp.Value);
+    }
+    
+    public override double SolvePuzzle2()
+    {
+        var data = LoadDataFromDay(9).Select(c => int.Parse(c.ToString())).ToList();
+        var length = data.Count;
+
+        var filledGroups = Enumerable.Range(0, length)
+            .Where(i => i % 2 == 0)
+            .Select(i => data[i])
+            .ToList();
+        
+        var idOrdered = GetIdNumberOrdered(filledGroups);
+        var refragementedfile = GetRefragmentedFile2(idOrdered, data);
+
+        return refragementedfile.Sum(kvp=> kvp.Key * kvp.Value);
+    }
+
+    private Dictionary<long, long> GetIdNumberOrdered(List<int> filledGroups)
+    {
+        List<(int size, int index)> sizeAndIndex = filledGroups.Zip(Enumerable.Range(0, filledGroups.Count)).ToList();
+
+        var filesOrdered = new Dictionary<long, long>();
+
+        var i = 0;
+
+        foreach (var group in sizeAndIndex)
+        {
+            for (var j = 0; j < group.size; j++)
+            {
+                filesOrdered.Add(i, group.index);
+                i += 1;
+            }
+        }
+
+        return filesOrdered;
+    }
+
+    private Dictionary<long, long> GetRefragmentedFile(Dictionary<long, long> idOrdered, List<int> input)
+    {
+        var fileamount = idOrdered.Count;
+        var refragmented = new Dictionary<long, long>();
+        var indexStart = 0;
+        var indexReverted = fileamount - 1;
+        var currentIndex = 0;
+        var groupnumber = 0;
+
+        while (refragmented.Count < fileamount)
+        {
+            if (groupnumber % 2 == 0)
+            {
+                for (var i = 0; i < input[groupnumber]; i++)
+                {
+                    refragmented.Add(currentIndex, idOrdered[indexStart]);
+                    indexStart += 1;
+                    currentIndex += 1;
+                } 
+            }
+            else
+            {
+                for (var i = 0; i < input[groupnumber]; i++)
+                {
+                    refragmented.Add(currentIndex, idOrdered[indexReverted]);
+                    indexReverted -= 1;
+                    currentIndex += 1;
+                } 
+            }
+            
+            groupnumber += 1;
+        }
+
+        return refragmented.Where(r=>r.Key < fileamount).ToDictionary(r=>r.Key, r=>r.Value);
+    }
+    
+    private Dictionary<long, long> GetRefragmentedFile2(Dictionary<long, long> idOrdered, List<int> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private Dictionary<long, int?> CreateInitialFileDictionnary(List<int> input)
+    {
+        var dictionary = new Dictionary<long, int?>();
+        var groupnumber = 0;
+        var index = 0;
+
+        foreach (var groupsize in input)
+        {
+            if (groupnumber % 2 == 0)
+            {
+                for (var i = 0; i < input[groupnumber]; i++)
+                {
+                    dictionary.Add(index, groupnumber);
+                    index += 1;
+                }
+                
+                groupnumber += 1;
+            }
+            else
+            {
+                for (var i = 0; i < input[groupnumber]; i++)
+                {
+                    dictionary.Add(index, null);
+                    index += 1;
+                }
+            }
+        }
+
+        return dictionary;
+    }
+}
